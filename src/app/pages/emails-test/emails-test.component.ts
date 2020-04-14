baed043a1835606d350efea9b9aa0a1621702422
Malikov Denis <ryki.admirala@gmail.com>
2020-04-14 23:03:03 +0300

    add email test: Leave Review

--- src/app/pages/emails-test/emails-test.component.ts
+++ src/app/pages/emails-test/emails-test.component.ts
@@ -50,6 +50,18 @@ export class EmailsTestComponent implements OnInit {
       .subscribe();
   }
 
+  sendLeaveReviewEmail() {
+    if (!this.checkEmail()) { return; }
+
+    const orderId = prompt(`Введите ID заказа`);
+    if (!orderId) { return; }
+
+    const apiUrl = `${API_HOST}/api/v1/${this.apiPrefix}/leave-review/${orderId}`;
+    this.http.post(apiUrl, { email: this.email })
+      .pipe(this.notyService.attachNoty({ successText: 'Письмо успешно отправлено' }))
+      .subscribe();
+  }
+
   private checkEmail(): boolean {
     if (this.email) {
       return true;
