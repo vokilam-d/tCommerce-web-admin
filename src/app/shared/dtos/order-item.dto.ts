8e883589cacf20b9bf8fb06f7fb83e4f9a647b1a
Malikov Denis <ryki.admirala@gmail.com>
2020-08-04 21:47:25 +0300

    [KL-277] add sku and vendorCode to OrderItem

--- src/app/shared/dtos/order-item.dto.ts
+++ src/app/shared/dtos/order-item.dto.ts
@@ -3,6 +3,7 @@ export class OrderItemDto {
   productId: number = 0;
   variantId: string = '';
   sku: string = '';
+  vendorCode: string = '';
   originalPrice: number = 0;
   price: number = 0;
   qty: number = 0;
