0512932c3e7b7c8eaa5940c4a425a2dd2167218c
Anastasiia Lysenko <lanastasialysenkol@gmail.com>
2021-02-18 22:21:58 +0200

    [KL-446] refactor code

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 
 export interface INavBarItem {
@@ -6,7 +7,6 @@ export interface INavBarItem {
   link: string;
   icon: string;
   isChildrenVisible?: boolean;
-  isSelected?: boolean;
   isPinned?: boolean;
   subItems?: INavBarItem[];
 }
@@ -19,150 +19,12 @@ export interface INavBarItem {
 })
 export class NavbarComponent implements OnInit {
 
-  public navBarMenu: INavBarItem[] = [];
+  public navBarMenu: INavBarItem[] = navBarMenu;
 
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
-    this.setMenu();
-  }
-
-  private setMenu() {
-    this.navBarMenu = [
-      {
-        name: 'Dashboard',
-        link: '/admin/dashboard',
-        icon: 'navbar__icon ai ai-dashboard',
-        isSelected: true
-      },
-      {
-        name: 'Продажи',
-        link: '',
-        icon: 'navbar__icon ai ai-sales',
-        isChildrenVisible: false,
-        isSelected: false,
-        subItems: [
-          {
-          name: 'Заказы',
-          link: '/admin/order',
-          icon: 'navbar__icon ai ai-sales',
-          isSelected: false,
-        },
-          {
-            name: 'Способы оплаты',
-            link: '/admin/payment-method',
-            icon: 'navbar__icon ai ai-sales',
-            isSelected: false,
-          }
-          ]
-      },
-      {
-        name: 'Каталог',
-        link: '',
-        icon: 'navbar__icon ai ai-catalog',
-        isChildrenVisible: false,
-        isSelected: false,
-        subItems: [
-            {
-              name: 'Категории',
-              link: '/admin/category',
-              icon: 'navbar__icon ai ai-catalog',
-              isSelected: false,
-            },
-            {
-              name: 'Товары',
-              link: '/admin/product',
-              icon: 'navbar__icon ai ai-catalog',
-              isSelected: false,
-            },
-            {
-              name: 'Атрибуты товаров',
-              link: '/admin/attribute',
-              icon: 'navbar__icon ai ai-catalog',
-              isSelected: false,
-            },
-            {
-              name: 'Доп. услуги',
-              link: '/admin/additional-service',
-              icon: 'navbar__icon ai ai-catalog',
-              isSelected: false,
-            },
-            {
-              name: 'Агрегаторы',
-              link: '/admin/aggregator',
-              icon: 'navbar__icon ai ai-catalog',
-              isSelected: false,
-            },
-          ]
-      },
-      {
-        name: 'Блог посты',
-        link: '/admin/blog-post',
-        icon: 'navbar__icon ai ai-content',
-        isSelected: false,
-      },
-      {
-        name: 'Клиенты',
-        link: '/admin/customer',
-        icon: 'navbar__icon ai ai-customers',
-        isSelected: false,
-      },
-      {
-        name: 'Отзывы',
-        link: '',
-        icon: 'navbar__icon ai ai-marketing',
-        isChildrenVisible: false,
-        isSelected: false,
-        subItems: [
-          {
-            name: 'Отзывы о магазине',
-            link: '/admin/store-review',
-            icon: 'navbar__icon ai ai-marketing',
-            isSelected: false,
-          },
-          {
-            name: 'Отзывы о товарах',
-            link: '/admin/product-review',
-            icon: 'navbar__icon ai ai-marketing',
-            isSelected: false,
-          }
-        ]
-      },
-      {
-        name: 'Система',
-        link: '',
-        icon: 'navbar__icon ai ai-system',
-        isChildrenVisible: false,
-        isSelected: false,
-        subItems: [
-          {
-            name: 'Объявление',
-            link: '/admin/announcement',
-            icon: 'navbar__icon ai ai-system',
-            isSelected: false,
-          },
-          {
-            name: 'Emails',
-            link: '/admin/emails-test',
-            icon: 'navbar__icon ai ai-system',
-            isSelected: false,
-          },
-          {
-            name: 'Валюты',
-            link: '/admin/currency',
-            icon: 'navbar__icon ai ai-system',
-            isSelected: false,
-          },
-          {
-            name: 'Админы',
-            link: '/admin/users',
-            icon: 'navbar__icon ai ai-system',
-            isSelected: false,
-          }
-        ]
-      }
-    ];
   }
 
   private getItemsWithChildren(): INavBarItem[] {
@@ -173,13 +35,9 @@ export class NavbarComponent implements OnInit {
     });
   }
 
-  private toggleActiveLinkClass(clickedItem: INavBarItem) {
-    this.navBarMenu.find(item => {
-      if (item === clickedItem) {
-        item.isSelected = !item.isSelected;
-      } else {
-        item.isSelected = false;
-      }
+  public isChildActive(item: INavBarItem): boolean {
+    return item.subItems?.some(subItem => {
+      return this.location.isCurrentPathEqualTo(subItem.link) && !subItem.isPinned;
     });
   }
 
@@ -192,18 +50,9 @@ export class NavbarComponent implements OnInit {
         item.isChildrenVisible = false;
       }
     });
-
-    this.toggleActiveLinkClass(clickedItem);
   }
 
-  public onSubItemClickHandler(clickedMenuItem: INavBarItem, subItem: INavBarItem) {
-    this.navBarMenu.find(item => {
-      if (item === subItem) {
-        item.isSelected = true;
-        clickedMenuItem.isSelected = false;
-      }
-  });
-
+  public onSubItemClickHandler(clickedMenuItem: INavBarItem) {
     clickedMenuItem.isChildrenVisible = false;
   }
 
@@ -211,7 +60,6 @@ export class NavbarComponent implements OnInit {
     const itemsWithChildren = this.getItemsWithChildren();
     itemsWithChildren.forEach(item => {
       item.isChildrenVisible = false;
-      item.isSelected = false;
     });
   }
 
@@ -230,5 +78,135 @@ export class NavbarComponent implements OnInit {
 
   public togglePinnedItem(subItem: INavBarItem) {
     subItem.isPinned = !subItem.isPinned;
+
+    const pinnedItems = this.getPinnedItems();
+    const storedPinnedItems = JSON.parse(localStorage.getItem('links'));
+
+    const isArraysEqual = this.compareArrays(pinnedItems, storedPinnedItems);
+
+    if (!isArraysEqual) {
+      localStorage.setItem('links', JSON.stringify(pinnedItems));
+    }
+  }
+
+  private compareArrays(pinnedItems: INavBarItem[], storedPinnedItems: INavBarItem[]): boolean {
+    return pinnedItems.every((item, index) => {
+      return item === storedPinnedItems[index];
+    });
   }
 }
+
+const navBarMenu = [
+  {
+    name: 'Dashboard',
+    link: '/admin/dashboard',
+    icon: 'navbar__icon ai ai-dashboard'
+  },
+  {
+    name: 'Продажи',
+    link: '',
+    icon: 'navbar__icon ai ai-sales',
+    isChildrenVisible: false,
+    subItems: [
+      {
+        name: 'Заказы',
+        link: '/admin/order',
+        icon: 'navbar__icon ai ai-sales'
+      },
+      {
+        name: 'Способы оплаты',
+        link: '/admin/payment-method',
+        icon: 'navbar__icon ai ai-sales'
+      }
+    ]
+  },
+  {
+    name: 'Каталог',
+    link: '',
+    icon: 'navbar__icon ai ai-catalog',
+    isChildrenVisible: false,
+    subItems: [
+      {
+        name: 'Категории',
+        link: '/admin/category',
+        icon: 'navbar__icon ai ai-catalog'
+      },
+      {
+        name: 'Товары',
+        link: '/admin/product',
+        icon: 'navbar__icon ai ai-catalog'
+      },
+      {
+        name: 'Атрибуты товаров',
+        link: '/admin/attribute',
+        icon: 'navbar__icon ai ai-catalog'
+      },
+      {
+        name: 'Доп. услуги',
+        link: '/admin/additional-service',
+        icon: 'navbar__icon ai ai-catalog'
+      },
+      {
+        name: 'Агрегаторы',
+        link: '/admin/aggregator',
+        icon: 'navbar__icon ai ai-catalog'
+      },
+    ]
+  },
+  {
+    name: 'Блог посты',
+    link: '/admin/blog-post',
+    icon: 'navbar__icon ai ai-content'
+  },
+  {
+    name: 'Клиенты',
+    link: '/admin/customer',
+    icon: 'navbar__icon ai ai-customers'
+  },
+  {
+    name: 'Отзывы',
+    link: '',
+    icon: 'navbar__icon ai ai-marketing',
+    isChildrenVisible: false,
+    subItems: [
+      {
+        name: 'Отзывы о магазине',
+        link: '/admin/store-review',
+        icon: 'navbar__icon ai ai-marketing'
+      },
+      {
+        name: 'Отзывы о товарах',
+        link: '/admin/product-review',
+        icon: 'navbar__icon ai ai-marketing'
+      }
+    ]
+  },
+  {
+    name: 'Система',
+    link: '',
+    icon: 'navbar__icon ai ai-system',
+    isChildrenVisible: false,
+    subItems: [
+      {
+        name: 'Объявление',
+        link: '/admin/announcement',
+        icon: 'navbar__icon ai ai-system'
+      },
+      {
+        name: 'Emails',
+        link: '/admin/emails-test',
+        icon: 'navbar__icon ai ai-system'
+      },
+      {
+        name: 'Валюты',
+        link: '/admin/currency',
+        icon: 'navbar__icon ai ai-system'
+      },
+      {
+        name: 'Админы',
+        link: '/admin/users',
+        icon: 'navbar__icon ai ai-system'
+      }
+    ]
+  }
+];
